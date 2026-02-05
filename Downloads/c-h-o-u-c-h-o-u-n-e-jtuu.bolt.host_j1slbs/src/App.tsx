03a94eeecbc726358b0bf140f8d3881d9ca4e29c
User <user@example.com>
2026-02-04 23:45:55 -0500

    "Cleanup"

--- Downloads/c-h-o-u-c-h-o-u-n-e-jtuu.bolt.host_j1slbs/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { PageWrapper } from "@/components/PageWrapper";
-
-export const App = () => {
-  return (
-    <body className="relative text-pink-900 text-base not-italic normal-nums font-normal accent-auto bg-white box-border caret-transparent block tracking-[0.96px] leading-6 list-outside list-disc min-h-[1000px] pointer-events-auto text-start indent-[0px] normal-case visible border-separate font-amandine before:accent-auto before:bg-[url('/images/background.png')] before:bg-center before:bg-no-repeat before:bg-cover before:box-border before:caret-transparent before:text-pink-900 before:block before:text-base before:not-italic before:normal-nums before:font-normal before:h-full before:tracking-[0.96px] before:leading-6 before:list-outside before:list-disc before:opacity-40 before:pointer-events-none before:fixed before:text-start before:indent-[0px] before:normal-case before:visible before:w-full before:z-0 before:border-separate before:left-0 before:top-0 before:font-amandine after:accent-auto after:bg-none after:box-border after:caret-transparent after:text-pink-900 after:block after:text-base after:not-italic after:normal-nums after:font-normal after:h-[600px] after:tracking-[0.96px] after:leading-6 after:list-outside after:list-disc after:opacity-15 after:pointer-events-none after:fixed after:text-start after:indent-[0px] after:normal-case after:visible after:w-[300px] after:z-0 after:border-separate after:right-[5%] after:bottom-0 after:font-amandine">
-      <PageWrapper />
-    </body>
-  );
-};
